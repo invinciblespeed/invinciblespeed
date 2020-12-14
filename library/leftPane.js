84d558dae3c28a576d2be3bb05aa15d3d5e708a6
invinciblespeed <43712584+invinciblespeed@users.noreply.github.com>
2020-12-14 16:50:43 -0500

    Here we go

---
+++ library/leftPane.js
@@ -0,0 +1,148 @@
+/*
+	<a>Overworld</a>
+	<br>
+	<span></span><a>Earth</a>
+	<br>
+	<a>The Void</a>
+*/
+
+const navPane = document.getElementById("navPane");
+const a = document.createElement("a");
+const br = document.createElement("br");
+const span = document.createElement("span");
+
+a.textContent = "Overworld";
+a.href = "overworld.html";
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Earth";
+a.href = "earth.html";
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Home";
+a.href = "home.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Desert";
+a.href = "desert.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Cave";
+a.href = "cave.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Campsite";
+a.href = "campsite.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Mountain";
+a.href = "mountain.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Nightlight";
+a.href = "nightlight.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Volcano";
+a.href = "volcano.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Ocean";
+a.href = "ocean.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Temple";
+a.href = "temple.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Old Laboratory";
+a.href = "old_laboratory.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Waterfall";
+a.href = "waterfall.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Nexus";
+a.href = "nexus.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "New Old Laboratory";
+a.href = "new_old_laboratory.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Mountain";
+a.href = "mountain.html";
+navPane.append(span.cloneNode());
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Moon";
+a.href = "moon.html";
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Void";
+a.href = "void.html";
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Void Gate";
+a.href = "void_gate.html";
+navPane.append(span.cloneNode());
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "Spirit Realm";
+a.href = "spirit_realm.html";
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
+
+a.textContent = "X̴̢̘͉̭͗̿̚4̶̖͇̯͊̍͜͝N̴̼̙̅̊7̸͓́̋̽̏Ḩ̷̢̬̀̑͝0̴̟̹̲͆x̷̢̣̑3̶͕̘͋͊́̕x̵̡͖̑̽̒̉7̵͉̰̮͐̀͑x̴̧̗̣̘̀̌5̸͓̱̑͐";
+a.href = "xander.html";
+navPane.append(a.cloneNode(true));
+navPane.append(br.cloneNode());
